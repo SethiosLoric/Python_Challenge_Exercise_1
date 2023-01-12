58a63220f48f5d4b36e2ae2e9d9b35f5ba29f37d
Sethios Busanga <109705416+SethiosLoric@users.noreply.github.com>
2023-01-12 15:46:28 +0100

    Update fibonacci.py
    ajout des commentaires pour l'explication du projet

--- fibonacci.py
+++ fibonacci.py
@@ -6,13 +6,13 @@
 #definition de la fonction de Fibonacci
 def fibonacci (n):
     base=0 #la variable base nous servira a unitialiser la base de la suite
-    unitial = 1
+    unitial = 1 #unitialisation du pas principal
     
-    while base < n :
+    while base < n : # la boucle while fait fonction d'itterateur
         print(base)
-        base, unitial = unitial, base + unitial
+        base, unitial = unitial, base + unitial # on affecte respectivement dans base la valeur de unitial et dans unitial la valeur de base plus unitial
         
         
-print('Bienvenue !')
-nombre = int(input('Enter a value :'))
-fib = fibonacci (nombre)
+print('Bienvenue dans cette Exercice !')
+nombre = int(input('Enter une valeur :')) #on demander d'entrer une valeur 
+fib = fibonacci (nombre) #on affiche le resultat
